8431cd17584781f4ba8a972f043c8e98896a985b
B-a-l-aj-i <balajivijay679@gmail.com>
2025-06-19 11:57:22 +0530

    feat: :sparkles: set viewport and user agent for Instagram screenshots

--- src/app/try/route.js
+++ src/app/try/route.js
@@ -263,7 +263,11 @@ export async function GET(request) {
               let  screenshotTarget= await page.$("header");
               await page.keyboard.press("Escape");
               if (urlStr.includes("instagram.com")) {
-                // await page.setViewport({ width: 400, height: 1080 });
+
+                await page.setViewport({ width: 400, height: 1080 });
+                await page.setUserAgent(
+                  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+                );
               }
               if (urlStr.includes("/reel/") || urlStr.includes("/p/")) { 
                 screenshotTarget = await page.$("article");
