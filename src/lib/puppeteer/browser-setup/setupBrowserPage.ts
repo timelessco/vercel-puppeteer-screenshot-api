3f46e56e6db9dc7cf7c4d38f16d3c6f8f8679650
B-a-l-aj-i <balajivijay679@gmail.com>
2025-08-20 16:36:35 +0530

    refactor: add user agent

--- src/lib/puppeteer/browser-setup/setupBrowserPage.ts
+++ src/lib/puppeteer/browser-setup/setupBrowserPage.ts
@@ -38,6 +38,9 @@ export async function setupBrowserPage(
 
 	await page.setViewport(viewport);
 	await page.emulateMediaFeatures(mediaFeatures);
+	await page.setUserAgent(
+		"Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+	);
 
 	if (enableAntiDetection) {
 		// JavaScript-level anti-detection evasions
