a261800ef6d51eb13c654b7f5a2c3115e9f67823
B-a-l-aj-i <balajivijay679@gmail.com>
2025-08-21 11:48:35 +0530

    refactor: remove networkidle2 in page.got

--- src/lib/puppeteer/navigation/navigationUtils.ts
+++ src/lib/puppeteer/navigation/navigationUtils.ts
@@ -4,6 +4,8 @@ import type { GetOrCreatePageReturnType } from "@/lib/puppeteer/browser/pageUtil
 import { getErrorMessage } from "@/utils/errorUtils";
 import type { GetScreenshotOptions } from "@/app/try/route";
 
+import { INSTAGRAM } from "../core/constants";
+
 interface GotoPageOptions {
 	fontTimeout?: number;
 	logger: GetScreenshotOptions["logger"];
@@ -38,7 +40,9 @@ export async function gotoPage(
 			timeout: navigationTimeout,
 			waitUntil: isDirectImage
 				? ["networkidle0"]
-				: ["domcontentloaded", "networkidle2"],
+				: url.includes(INSTAGRAM)
+					? ["domcontentloaded"]
+					: ["domcontentloaded", "networkidle2"],
 		});
 
 		// For non-image pages, wait for fonts to load
