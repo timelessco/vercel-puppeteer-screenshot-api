0407953db10c3b15e5c654dde3470c9302b8f969
Navin Moorthy <navin007.a@gmail.com>
2025-08-22 17:51:48 +0530

    feat: enhance Puppeteer stealth capabilities by adding more evasion plugins

--- src/lib/puppeteer/browser/launchBrowser.ts
+++ src/lib/puppeteer/browser/launchBrowser.ts
@@ -2,7 +2,22 @@ import { addExtra, PuppeteerExtra } from "puppeteer-extra";
 import PuppeteerExtraPluginStealth from "puppeteer-extra-plugin-stealth";
 
 import "puppeteer-extra-plugin-stealth/evasions/chrome.app";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.csi";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.runtime";
+import "puppeteer-extra-plugin-stealth/evasions/defaultArgs";
+import "puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow";
+import "puppeteer-extra-plugin-stealth/evasions/media.codecs";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.languages";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.permissions";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.plugins";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.vendor";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.webdriver";
+import "puppeteer-extra-plugin-stealth/evasions/sourceurl";
 import "puppeteer-extra-plugin-stealth/evasions/user-agent-override";
+import "puppeteer-extra-plugin-stealth/evasions/webgl.vendor";
+import "puppeteer-extra-plugin-stealth/evasions/window.outerdimensions";
 
 import type { LaunchOptions } from "rebrowser-puppeteer-core";
 
@@ -173,6 +188,7 @@ export async function launchBrowser(options: LaunchBrowserOptions) {
 		// @ts-expect-error - Type incompatibility between rebrowserPuppeteer and puppeteer-extra
 		puppeteer = addExtra(rebrowserPuppeteer);
 		const stealth = PuppeteerExtraPluginStealth();
+		console.log("🚀 ~ launchBrowser ~ stealth:", stealth.enabledEvasions);
 		// @ts-expect-error - Type incompatibility between rebrowserPuppeteer and puppeteer-extra
 		const browser = await puppeteer.use(stealth).launch(launchOptions);
 
